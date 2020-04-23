546026b278585ee9175361fe7cd162e937b65f15
diaoqi <diaoqi@diaoqi.local>
2020-04-23 11:45:33 +0800

    build h5

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ const config = {
     '828': 1.81 / 2
   },
   sourceRoot: 'src',
-  outputRoot: 'dist',
+  outputRoot: process.env.TARO_ENV === 'h5' ? 'h5dist' : 'dist',
   plugins: {
     babel: {
       sourceMap: true,
---
+++ h5dist/chunk/login_index.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{"157":function(n,w){}}]);
